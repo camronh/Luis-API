6d3249e37a829f5bb78f4d5936cbd43b663069e2
LuisESuperlano <lesuperlano@gmail.com>
2021-10-20 14:01:58 -0400

    First End Point

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
-
+const port = 3000;
 // start server
+
+app.get("/randomNumber", async function (req, res) {
+  console.log("request reciept")
+  console.log(req.query)
+  res.send("Its Alive")
+
+
+});
+
 app.listen(port, function () {
   console.log("listening on http://localhost:" + port);
 });
