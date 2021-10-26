2dd141eaadc264bb1b001ec020c13d3779c34d52
LuisESuperlano <lesuperlano@gmail.com>
2021-10-26 11:12:35 -0400

    claudia

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const app = express();
 const port = 3000;
 // start server
 
-app.get("/randomNumber", async function (req, res) {
+app.get("/randomNumber"), async function (req, res) {
   console.log("request reciept")
+  const min = Number(req.query.min)
+  const max = Number(req.query.max)
+  let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min
   console.log(req.query)
-  res.send("Its Alive")
-
-
+  res.send(`${randomNumber}`)
 });
 
-app.listen(port, function () {
-  console.log("listening on http://localhost:" + port);
-});
+// app.listen(port, function () {
+//   console.log("listening on http://localhost:" + port);
+// });
+module.exports = app;
