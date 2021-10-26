27cc118aa87a2369d29c2119c6be8ebf09a2cf1f
camronh <camronhaider@gmail.com>
2021-10-26 11:10:17 -0400

    Random Word API now

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
+// Import the .env
+require("dotenv").config();
+const API_KEY = process.env.API_KEY;
+
+// Import libraries
+const randomWords = require("random-words");
 const express = require("express");
+
+// Set constants
 const app = express();
 const port = 3000;
-// start server
 
-app.get("/randomNumber", async function (req, res) {
-  console.log("request reciept")
-  console.log(req.query)
-  res.send("Its Alive")
-
-
+// GET: Get Random Number
+// Query Params: apiKey
+app.get("/randomWord", async function (req, res) {
+  const { apiKey } = req.query;
+  if (apiKey !== API_KEY) return res.status(500).send("Invalid API key");
+  const randomWord = randomWords();
+  res.status(200).send({ randomWord });
 });
 
+// Start Server
 app.listen(port, function () {
   console.log("listening on http://localhost:" + port);
 });
+
+// Include this line to use claudia
+module.exports = app;
